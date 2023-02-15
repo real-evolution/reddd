657aeb68e009a1660738496b83abf9d752c9fe8c
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-15 23:52:47 +0300

    test(value_type): test `ValueType` derive macro

--- reddd/src/domain/value_type.rs
+++ reddd/src/domain/value_type.rs
@@ -4,3 +4,90 @@ pub trait ValueType {
     fn value(self) -> Self::Value;
     fn value_ref(&self) -> &Self::Value;
 }
+
+#[cfg(test)]
+mod tests {
+    use fake::{Dummy, Fake, Faker};
+    use reddd_macros::ValueType;
+
+    use super::*;
+
+    #[test]
+    fn simple_newtype_annotated_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType(#[main_field] i32);
+
+        let f: NewType = Faker.fake();
+        let fval = f.0;
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+
+    #[test]
+    fn simple_newtype_auto_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType(i32);
+
+        let f: NewType = Faker.fake();
+        let fval = f.0;
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+
+    #[test]
+    fn multifield_newtype_annotated_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType(i32, #[main_field] String);
+
+        let f: NewType = Faker.fake();
+        let fval = f.1.clone();
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+
+    #[test]
+    fn multifield_newtype_auto_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType(i32, String);
+
+        let f: NewType = Faker.fake();
+        let fval = f.0;
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+
+    #[test]
+    fn multifield_named_annotated_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType {
+            _field0: i32,
+            #[main_field]
+            field1: String,
+        }
+
+        let f: NewType = Faker.fake();
+        let fval = f.field1.clone();
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+
+    #[test]
+    fn multifield_named_auto_test() {
+        #[derive(ValueType, Dummy)]
+        struct NewType {
+            field0: i32,
+            _field1: String,
+        }
+
+        let f: NewType = Faker.fake();
+        let fval = f.field0;
+
+        assert_eq!(&fval, f.value_ref());
+        assert_eq!(fval, f.value());
+    }
+}
