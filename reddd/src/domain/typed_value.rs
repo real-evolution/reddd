e4b96a0573189fa586380394e0f46bb50920ac26
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-16 02:33:02 +0300

    doc(typed_value): document `TypedValue` struct

--- reddd/src/domain/typed_value.rs
+++ reddd/src/domain/typed_value.rs
@@ -4,6 +4,32 @@ use reddd_macros::ValueType;
 
 use crate::domain::ValueType;
 
+/// A value of type `V` wrapper that is typed to a specific type `T`
+///
+/// Two types with the same `V` types are compatible if used with a function
+/// that requires a [`ValueType<Value = V>`]. However, they **are not** when
+/// used with functions that requires either concretely.
+///
+/// # Generic Parameters
+///
+/// * `V`: Type of wrapped values
+/// * `T`: Type to distinguish wrappers with same `V` with
+///
+/// # Example
+/// ```rust
+/// type MyTypedValue1 = TypedValue<usize, MyFirstType>;
+/// type MyTypedValue2 = TypedValue<usize, MySecondType>;
+///
+/// // `MyTypedValue1` and `MyTypedValue2` can be used here
+/// fn generic_func<V: ValueType<Value = usize>>(value: V) {
+///     // --snip--
+/// }
+///
+/// // Only `MyTypedValue1` can be used here
+/// fn concrete_func(value: MyTypedValue1) {
+///     // --snip--
+/// }
+/// ````
 #[derive(Debug, ValueType)]
 pub struct TypedValue<V, T>(#[main_field] V, PhantomData<dyn Fn() -> T>)
 where
