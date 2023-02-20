8f09620ab51ceecb7b3cd3f54015d29963b8b8eb
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-20 23:34:15 +0300

    feat(macros): add a method to `FieldExt` to get the field identifier

--- reddd_macros/src/util/fields.rs
+++ reddd_macros/src/util/fields.rs
@@ -1,10 +1,16 @@
 pub(crate) trait FieldExt {
+    fn id(&self) -> Option<syn::Ident>;
+
     fn has_attribute(&self, attr: &str) -> bool;
 }
 
 macro_rules! impl_field_ext {
     ($type:ty) => {
         impl crate::util::FieldExt for $type {
+            fn id(&self) -> Option<syn::Ident> {
+                self.ident.clone()
+            }
+
             fn has_attribute(&self, attr: &str) -> bool {
                 self.attrs.iter().any(|a| {
                     a.path.get_ident().map(|id| id == attr).unwrap_or(false)
