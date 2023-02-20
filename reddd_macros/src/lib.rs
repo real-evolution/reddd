5bb5c53d685380277257f83f3169e164a752b786
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-20 23:35:35 +0300

    feat(macros): add a function that exposes `Entity` proc-macro implementation

--- reddd_macros/src/lib.rs
+++ reddd_macros/src/lib.rs
@@ -18,3 +18,18 @@ pub fn derive_value_type(
     .into_token_stream()
     .into()
 }
+
+#[proc_macro_error::proc_macro_error]
+#[proc_macro_derive(Entity, attributes(id_field, created_at_field))]
+pub fn derive_entity(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+
+    match entity::Entity::from_derive_input(&input) {
+        | Ok(tokens) => tokens,
+        | Err(err) => panic!("{err:?}"),
+    }
+    .into_token_stream()
+    .into()
+}
