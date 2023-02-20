b55a538e0f4e7a767781a4b7b50cdfb1c8970420
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-20 23:50:26 +0300

    refactor(macros): reduce repetition when exporting proc-macros

--- reddd_macros/src/lib.rs
+++ reddd_macros/src/lib.rs
@@ -4,32 +4,26 @@ mod entity;
 mod util;
 mod value_type;
 
-#[proc_macro_error::proc_macro_error]
-#[proc_macro_derive(ValueType, attributes(main_field))]
-pub fn derive_value_type(
-    input: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+macro_rules! define_proc_macro {
+    ($trait:ident #[ $($attr:ident),* ]) => {
+        paste::paste! {
+            #[proc_macro_error::proc_macro_error]
+            #[proc_macro_derive($trait, attributes($($attr),*))]
+            pub fn [<derive $trait:snake>](
+                input: proc_macro::TokenStream,
+            ) -> proc_macro::TokenStream {
+                let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    match value_type::ValueType::from_derive_input(&input) {
-        | Ok(tokens) => tokens,
-        | Err(err) => panic!("{err:?}"),
-    }
-    .into_token_stream()
-    .into()
+                match [<$trait:snake>]::$trait::from_derive_input(&input) {
+                    | Ok(tokens) => tokens,
+                    | Err(err) => panic!("{err:?}"),
+                }
+                .into_token_stream()
+                .into()
+            }
+        }
+    };
 }
 
-#[proc_macro_error::proc_macro_error]
-#[proc_macro_derive(Entity, attributes(id_field, created_at_field))]
-pub fn derive_entity(
-    input: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
-
-    match entity::Entity::from_derive_input(&input) {
-        | Ok(tokens) => tokens,
-        | Err(err) => panic!("{err:?}"),
-    }
-    .into_token_stream()
-    .into()
-}
+define_proc_macro!(ValueType #[ main_field ]);
+define_proc_macro!(Entity #[ id_field, created_at_field ]);
