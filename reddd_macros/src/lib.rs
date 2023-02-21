f44d16946c823d3945c14c8a7236a2f350738d58
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-21 11:55:02 +0300

    build(macros): export `MutableEntity` proc macro

--- reddd_macros/src/lib.rs
+++ reddd_macros/src/lib.rs
@@ -1,29 +1,58 @@
 use darling::{FromDeriveInput, ToTokens};
+use proc_macro::TokenStream;
 
 mod entity;
 mod util;
 mod value_type;
+
+macro_rules! parse_derive_input {
+    ($input:ident => $parser:ty) => {{
+        let input = syn::parse_macro_input!($input as syn::DeriveInput);
+
+        let ts: TokenStream = match <$parser>::from_derive_input(&input) {
+            | Ok(tokens) => tokens,
+            | Err(err) => return err.write_errors().into(),
+        }
+        .into_token_stream()
+        .into();
+
+        ts
+    }};
+}
 
 macro_rules! define_proc_macro {
     ($trait:ident #[ $($attr:ident),* ]) => {
         paste::paste! {
             #[proc_macro_error::proc_macro_error]
             #[proc_macro_derive($trait, attributes($($attr),*))]
-            pub fn [<derive $trait:snake>](
-                input: proc_macro::TokenStream,
-            ) -> proc_macro::TokenStream {
-                let input = syn::parse_macro_input!(input as syn::DeriveInput);
-
-                match [<$trait:snake>]::$trait::from_derive_input(&input) {
-                    | Ok(tokens) => tokens,
-                    | Err(err) => panic!("{err:?}"),
-                }
-                .into_token_stream()
-                .into()
+            pub fn [<derive_ $trait:snake>](input: TokenStream) -> TokenStream {
+                parse_derive_input!(input => [<$trait:snake>]::$trait).into()
             }
         }
     };
 }
 
+macro_rules! concat_token_streams {
+    ($($ts:expr),+) => {{
+        let mut ts = TokenStream::new();
+
+        $(ts.extend($ts);)*
+
+        ts
+    }}
+}
+
 define_proc_macro!(ValueType #[ main_field ]);
 define_proc_macro!(Entity #[ id_field, created_at_field ]);
+
+#[proc_macro_derive(
+    MutableEntity,
+    attributes(id_field, created_at_field, updated_at_field)
+)]
+#[proc_macro_error::proc_macro_error]
+pub fn derive_mutable_entity(input: TokenStream) -> TokenStream {
+    concat_token_streams!(
+        derive_entity(input.clone()),
+        parse_derive_input!(input => entity::MutableEntity)
+    )
+}
