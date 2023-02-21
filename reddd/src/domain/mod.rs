8717c3ef311584c76180dd03126746b6416993d7
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-21 12:00:35 +0300

    refactor(domain): move `typed_value` module code to `value_type` module

--- reddd/src/domain/mod.rs
+++ reddd/src/domain/mod.rs
@@ -1,7 +1,5 @@
 mod entity;
-mod typed_value;
 mod value_type;
 
 pub use entity::*;
-pub use typed_value::*;
 pub use value_type::*;
--- reddd/src/domain/value_type.rs
+++ reddd/src/domain/value_type.rs
@@ -1,3 +1,7 @@
+use std::marker::PhantomData;
+
+use reddd_macros::ValueType;
+
 /// A trait to be implemented by concrete value types.
 ///
 /// This trait can be useful to "embed" domain rules (such as validation)
@@ -30,6 +34,46 @@ pub trait ValueType: Clone {
 
     /// Borrows a reference to the wrapped value
     fn value_ref(&self) -> &Self::Value;
+}
+
+/// A value of type `V` wrapper that is typed to a specific type `T`
+///
+/// Two types with the same `V` types are compatible if used with a function
+/// that requires a [`ValueType<Value = V>`]. However, they **are not** when
+/// used with functions that requires either concretely.
+///
+/// # Generic Parameters
+///
+/// * `V`: Type of wrapped values
+/// * `T`: Type to distinguish wrappers with same `V` with
+///
+/// # Example
+/// ```rust
+/// type MyTypedValue1 = TypedValue<usize, MyFirstType>;
+/// type MyTypedValue2 = TypedValue<usize, MySecondType>;
+///
+/// // `MyTypedValue1` and `MyTypedValue2` can be used here
+/// fn generic_func<V: ValueType<Value = usize>>(value: V) {
+///     // --snip--
+/// }
+///
+/// // Only `MyTypedValue1` can be used here
+/// fn concrete_func(value: MyTypedValue1) {
+///     // --snip--
+/// }
+/// ````
+#[derive(Debug, ValueType)]
+pub struct TypedValue<V, T>(#[main_field] V, PhantomData<dyn Fn() -> T>)
+where
+    V: Clone + std::fmt::Debug + PartialEq + PartialOrd;
+
+impl<V, T> Clone for TypedValue<V, T>
+where
+    V: Clone + std::fmt::Debug + PartialEq + PartialOrd,
+{
+    fn clone(&self) -> Self {
+        Self(self.0.clone(), self.1)
+    }
 }
 
 #[cfg(test)]
