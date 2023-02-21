067e31031594a0554e5028bd76158a995d65112a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-21 11:54:43 +0300

    feat(macros): add a macro to implement `MutableEntity` trait

--- reddd_macros/src/entity.rs
+++ reddd_macros/src/entity.rs
@@ -1,10 +1,23 @@
-use crate::util::StructExt;
 use darling::{FromDeriveInput, ToTokens};
+
+use crate::util::StructExt;
 
 #[derive(Debug, FromDeriveInput)]
 #[darling(forward_attrs(allow, doc, cfg))]
 #[darling(attributes(id_field, created_at_field), supports(struct_named))]
 pub(super) struct Entity {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    data: darling::ast::Data<(), syn::Field>,
+}
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(forward_attrs(allow, doc, cfg))]
+#[darling(
+    attributes(id_field, created_at_field, updated_at_field),
+    supports(struct_named)
+)]
+pub(super) struct MutableEntity {
     ident: syn::Ident,
     generics: syn::Generics,
     data: darling::ast::Data<(), syn::Field>,
@@ -41,6 +54,37 @@ impl ToTokens for Entity {
 
                 fn created_at(&self) -> &DateTime<Utc> {
                     &self.#created_at_ident
+                }
+            }
+        });
+    }
+}
+
+impl ToTokens for MutableEntity {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let MutableEntity {
+            ref ident,
+            ref generics,
+            ref data,
+        } = *self;
+
+        let (imp, ty, wher) = generics.split_for_impl();
+
+        let updated_at_ident = &data
+            .get_field_by_attr_or_id("updated_at_field", "updated_at")
+            .expect("`updated_at` field is required")
+            .ident;
+
+        tokens.extend(quote::quote! {
+            impl #imp MutableEntity for #ident #ty #wher {
+                fn updated_at(&self) -> &DateTime<Utc> {
+                    &self.#updated_at_ident
+                }
+
+                fn touch(&mut self) -> &DateTime<Utc> {
+                    self.#updated_at_ident = Utc::now();
+
+                    &self.updated_at()
                 }
             }
         });
