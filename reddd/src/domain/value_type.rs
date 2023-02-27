d42709d2b96c6b28ece850a5ace6b9156c92f534
Ayman Al-Qadhi <alqd@protonmail.com>
2023-02-27 22:52:33 +0300

    feat: make `TypedValue` compatible with `serde` if `serde` feature is enabled

--- reddd/src/domain/value_type.rs
+++ reddd/src/domain/value_type.rs
@@ -58,41 +58,83 @@ pub trait ValueType: Clone {
     fn as_inner(&self) -> &Self::Inner;
 }
 
-/// A value of type `V` wrapper that is typed to a specific type `T`
-///
-/// Two types with the same `V` types are compatible if used with a function
-/// that requires a [`ValueType<Value = V>`]. However, they **are not** when
-/// used with functions that requires either concretely.
-///
-/// # Generic Arguments
-///
-/// * `V` - Type of wrapped values
-/// * `T` - Type to distinguish wrappers with same `V` with
-///
-/// # Example
-/// ```rust
-/// use reddd::domain::{TypedValue, ValueType};
-///
-/// struct MyFirstType;
-/// struct MySecondType;
-///
-/// type MyTypedValue1 = TypedValue<usize, MyFirstType>;
-/// type MyTypedValue2 = TypedValue<usize, MySecondType>;
-///
-/// // `MyTypedValue1` and `MyTypedValue2` can be used here
-/// fn generic_func<V: ValueType<Inner = usize>>(value: V) {
-///     // --snip--
-/// }
-///
-/// // Only `MyTypedValue1` can be used here
-/// fn concrete_func(value: MyTypedValue1) {
-///     // --snip--
-/// }
-/// ````
-#[derive(Debug, ValueType)]
-pub struct TypedValue<V, T>(#[main_field] V, PhantomData<dyn Fn() -> T>)
-where
-    V: Clone + std::fmt::Debug + PartialEq + PartialOrd;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "serde")] {
+        /// A serde-compatible value of type `V` wrapper that is typed to a
+        /// specific type `T`.
+        ///
+        /// Two types with the same `V` types are compatible if used with a function
+        /// that requires a [`ValueType<Value = V>`]. However, they **are not** when
+        /// used with functions that requires either concretely.
+        ///
+        /// # Generic Arguments
+        ///
+        /// * `V` - Type of wrapped values
+        /// * `T` - Type to distinguish wrappers with same `V` with
+        ///
+        /// # Example
+        /// ```rust
+        /// use reddd::domain::{TypedValue, ValueType};
+        ///
+        /// struct MyFirstType;
+        /// struct MySecondType;
+        ///
+        /// type MyTypedValue1 = TypedValue<usize, MyFirstType>;
+        /// type MyTypedValue2 = TypedValue<usize, MySecondType>;
+        ///
+        /// // `MyTypedValue1` and `MyTypedValue2` can be used here
+        /// fn generic_func<V: ValueType<Inner = usize>>(value: V) {
+        ///     // --snip--
+        /// }
+        ///
+        /// // Only `MyTypedValue1` can be used here
+        /// fn concrete_func(value: MyTypedValue1) {
+        ///     // --snip--
+        /// }
+        /// ````
+        #[derive(Debug, ValueType, serde::Serialize, serde::Deserialize)]
+        #[serde(transparent)]
+        pub struct TypedValue<V, T>(#[main_field] V, PhantomData<dyn Fn() -> T>)
+        where
+            V: Clone + std::fmt::Debug + PartialEq + PartialOrd;
+    } else {
+        /// A value of type `V` wrapper that is typed to a specific type `T`.
+        ///
+        /// Two types with the same `V` types are compatible if used with a function
+        /// that requires a [`ValueType<Value = V>`]. However, they **are not** when
+        /// used with functions that requires either concretely.
+        ///
+        /// # Generic Arguments
+        ///
+        /// * `V` - Type of wrapped values
+        /// * `T` - Type to distinguish wrappers with same `V` with
+        ///
+        /// # Example
+        /// ```rust
+        /// use reddd::domain::{TypedValue, ValueType};
+        ///
+        /// struct MyFirstType;
+        /// struct MySecondType;
+        ///
+        /// type MyTypedValue1 = TypedValue<usize, MyFirstType>;
+        /// type MyTypedValue2 = TypedValue<usize, MySecondType>;
+        ///
+        /// // `MyTypedValue1` and `MyTypedValue2` can be used here
+        /// fn generic_func<V: ValueType<Inner = usize>>(value: V) {
+        ///     // --snip--
+        /// }
+        ///
+        /// // Only `MyTypedValue1` can be used here
+        /// fn concrete_func(value: MyTypedValue1) {
+        ///     // --snip--
+        /// }
+        /// ````
+        #[derive(Debug, ValueType)]
+        pub struct TypedValue<V, T>(#[main_field] V, PhantomData<dyn Fn() -> T>)
+        where
+            V: Clone + std::fmt::Debug + PartialEq + PartialOrd;
+    }
+}
 
 impl<V, T> TypedValue<V, T>
 where
@@ -249,5 +291,27 @@ mod tests {
         assert!(f1 <= f2);
         assert!(f2 > f1);
         assert!(f2 >= f1);
+    }
+
+    #[test]
+    #[cfg(feature = "serde")]
+    fn serde_test() {
+        #[derive(Clone, Debug)]
+        struct SomeType;
+
+        type TestType = TypedValue<i64, SomeType>;
+
+        let inner = rand::random();
+
+        let value = TestType::new(inner);
+        assert_eq!(&inner, value.as_inner());
+
+        let serialized = serde_json::to_string(&value).unwrap();
+        assert_eq!(format!(r#"{inner}"#), serialized);
+
+        let deserialized: TestType = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(value, deserialized);
+        assert_eq!(value.as_inner(), deserialized.as_inner());
+        assert_eq!(value.into_inner(), deserialized.into_inner());
     }
 }
