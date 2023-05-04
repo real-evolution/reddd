41efdde8df464a84f488daa71b7a82e48489fb8c
Ayman Al-Qadhi <alqd@protonmail.com>
2023-05-04 18:35:28 +0300

    build: make `usecase` module toggleable via a cargo feature

--- reddd/src/domain/mod.rs
+++ reddd/src/domain/mod.rs
@@ -1,9 +1,13 @@
 mod entity;
 mod repo;
 mod value_type;
+
+#[cfg(feature = "usecase")]
 mod usecase;
 
 pub use entity::*;
 pub use repo::*;
 pub use value_type::*;
+
+#[cfg(feature = "usecase")]
 pub use usecase::*;
