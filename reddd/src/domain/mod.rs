775c9482ca6d6cfa73ca60cf8e7599a240103ae4
Ayman Al-Qadhi <alqd@protonmail.com>
2023-05-04 18:33:11 +0300

    feat: add use-case traits

--- reddd/src/domain/mod.rs
+++ reddd/src/domain/mod.rs
@@ -1,7 +1,9 @@
 mod entity;
 mod repo;
 mod value_type;
+mod usecase;
 
 pub use entity::*;
 pub use repo::*;
 pub use value_type::*;
+pub use usecase::*;
---
+++ reddd/src/domain/usecase.rs
@@ -0,0 +1,33 @@
+/// A trait to be implemented by use cases.
+pub trait UseCase {
+    /// The input to the use case.
+    type Input;
+
+    /// The output of the use case.
+    type Output;
+}
+
+/// A trait to be implemented by use case handlers.
+///
+/// # Parameters
+///
+/// * `U` - The type of the use case to be handled.
+/// * `S` - The type of the state of the application.
+#[async_trait::async_trait]
+pub trait UseCaseHandler<U: UseCase, S> {
+    /// The type of the error that can occur during the use case execution.
+    type Error: Send + Sync;
+
+    /// Asynchronously executes the use case.
+    ///
+    /// # Parameters
+    /// - `input` - The input to the use case.
+    /// - `state` - The state of the application.
+    ///
+    /// # Returns
+    /// The output of the use case.
+    async fn execute(
+        input: U::Input,
+        state: &S,
+    ) -> Result<U::Output, Self::Error>;
+}
